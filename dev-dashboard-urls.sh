7d4186e6647cbc8d024f2c3d11044b323e1093fd
Brian Mason <bmason@equalexperts.com>
2019-01-17 09:27:34 +0000

    added github status page

--- dev-dashboard-urls.sh
+++ dev-dashboard-urls.sh
@@ -1,8 +1,9 @@
 
 TRAVIS_BUILD_LIST_ALL="https://travis-ci.com/DepartmentOfHealth-htbhf/"
 GITHUB_BUILD_LIST_ALL="https://github.com/DepartmentOfHealth-htbhf"
+GITHUB_PROJECT_STATUS="https://departmentofhealth-htbhf.github.io/"
 
 
-URLS=( ${TRAVIS_BUILD_LIST_ALL} {GITHUB_BUILD_LIST_ALL} )
+URLS=( ${TRAVIS_BUILD_LIST_ALL} ${GITHUB_BUILD_LIST_ALL} ${GITHUB_PROJECT_STATUS})
 
 
